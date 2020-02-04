f9d1bc9567c26a65ba2e6dfe3478e0f036b6eec8
David Turner <davidpaulturner350@gmail.com>
2020-02-04 21:29:06 +0000

    added simple cnn with two conv layers

--- nets/network_models/pytorch.py
+++ nets/network_models/pytorch.py
@@ -21,12 +21,6 @@ def data_loader(path, batch_size=10):
     return dataset_loader
 
 
-class cifar10_dataset:
-
-    def __init__(self):
-        pass
-
-
 class SimpleNet(nn.Module):
     def __init__(self):
         super(SimpleNet, self).__init__()
@@ -70,7 +64,60 @@ class SimpleNet(nn.Module):
 
 
 class SimpleCNN(nn.Module):
+    """
+    Calculating the image size after a pooling function =
+
+        W_out = (W_in - poolingfilter_size + 2 * poolingfilter_padding)/
+                (poolingfilter_stride)                                       + 1
+
+        so if a max pooling filter of stride 2 and width 2 and no padding the image width is halved
+    """
+
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
-        
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.ReLU = nn.ReLU(inplace=True)
+        self.fc1 = nn.Linear(in_features=64*8*8, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=10)
+
+    def forward(self, input):
+        out = self.conv1(input)
+        out = self.bn1(out)
+        out = self.pool(out)
+        out = self.ReLU(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.pool(out)
+        out = self.ReLU(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc1(out)
+        x = self.fc2(out)
+        return x
+
+    def train(self, train_loader, criterion, optimizer, epochs):
+        print('Starting Training')
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i, data in enumerate(train_loader, 0):
+
+                inputs, labels = data
+
+                self.zero_grad()
+                outs = self(inputs)
+                loss = criterion(outs, labels)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+                if i % 250 == 249:    # print every 2000 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, i + 1, running_loss / 250))
+                    running_loss = 0.0
+
+        print('Finished Training')
