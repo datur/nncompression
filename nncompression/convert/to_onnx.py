f7f50190bbd8c7bee219d1308de1241b9867536c
Dave <davidpaulturner350@gmail.com>
2020-06-08 14:23:51 +0100

    added new functionality

--- nncompression/convert/to_onnx.py
+++ nncompression/convert/to_onnx.py
@@ -2,12 +2,15 @@
 """
 import onnx
 import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 import onnxruntime
 import numpy as np
-from ..utils import to_numpy
+from ..utils import to_numpy, DEVICE, BATCH_SIZE
 
 
-def from_pytorch(model, dummy_data, file_name, device):
+def from_pytorch(model, dummy_data, file_name):
     """A function to convert a pytorch model to an onnx model
     Once converted perform tests to ensure model is converted correctly
 
@@ -22,17 +25,21 @@ def from_pytorch(model, dummy_data, file_name, device):
 
     """
 
-    model.to(device)
+    model.to(DEVICE)
 
-    dummy_data = dummy_data.to(device)
+    # print(model)
+
+    dummy_data = dummy_data.to(DEVICE)
 
     out = model(dummy_data)
 
     torch.onnx.export(model, dummy_data, file_name, export_params=True,
-                      opset_version=11, do_constant_folding=True, input_names=['input'],
+                      opset_version=12, do_constant_folding=True, input_names=['input'],
                       output_names=['output'],
                       dynamic_axes={'input': {0: 'batch_size'},
-                                    'output': {0: 'batch_size'}})
+                                    'output': {0: 'batch_size'}},
+                      #   operator_export_type=torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK
+                      )
 
     onnx_model = onnx.load(file_name)
     onnx.checker.check_model(onnx_model)
@@ -42,8 +49,31 @@ def from_pytorch(model, dummy_data, file_name, device):
     ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(dummy_data)}
     ort_outs = ort_session.run(None, ort_inputs)
 
-    # compare ONNX Runtime and PyTorch results
+    # compare ONNX Runtime and PyTorch resultsr
     np.testing.assert_allclose(
-        to_numpy(out), ort_outs[0], rtol=1e-03, atol=1e-05)
+        to_numpy(out), ort_outs[0], rtol=1e-03, atol=2e-05)
 
     print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+
+
+def get_cifar_10_test_loader():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    testset = torchvision.datasets.CIFAR10(root='../data', train=False,
+                                           download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
+                                              shuffle=False, num_workers=2)
+
+    return test_loader
+
+
+def get_imagenet_val_loader():
+    transform = transforms.Compose(
+        [transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()])
+
+    dataset = datasets.ImageNet(".", split="val", transform=transform)
+    imgnet_val_loader = torch.utils.data.DataLoader(
+        dataset=dataset, batch_size=32, shuffle=False)
+
+    return imgnet_val_loader
--- nncompression/convert/to_openvino.py
+++ nncompression/convert/to_openvino.py
@@ -1,7 +1,22 @@
 """Module for converting neural network models to openvino intermediate representation
 """
 
+import subprocess
+import sys
 
-def from_onnx(model):
+
+def from_onnx(input_model: str, input_shape: list,  output_dir: str, mo_path: str):
+    """
+    A method for converting ONNX models into openvino IR
+
+    :param model: [description]
+    :type model: [type]
+
+    """
     # TODO [$5e7153761b7d3d0007b9e4d9]: convert provided onnx model to openvino represnetation
-    pass
+
+    args = ["python", f"{mo_path}", "--input_model", f"{input_model}",
+            "--input_shape", f"{input_shape}",
+            "--output_dir", f"{output_dir}"]
+
+    subprocess.Popen(args)
