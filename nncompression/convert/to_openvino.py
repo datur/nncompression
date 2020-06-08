f05f790142fc9dd9d411fd472b619d60cac2cb2a
TODO <todo-actions[bot]@users.noreply.github.com>
2020-06-08 13:33:35 +0000

    Update TODO references: #16, #17

--- nncompression/convert/to_openvino.py
+++ nncompression/convert/to_openvino.py
@@ -14,7 +14,7 @@ def from_onnx(input_model: str, input_shape: list,  output_dir: str, mo_path: st
     :type model: [type]
 
     """
-    # TODO [$5e7153761b7d3d0007b9e4d9]: convert provided onnx model to openvino represnetation
+    # TODO [#16]: convert provided onnx model to openvino represnetation
 
     args = ["python", f"{mo_path}", "--input_model", f"{input_model}",
             "--input_shape", f"{input_shape}",
