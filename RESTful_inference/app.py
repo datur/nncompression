62cbaf00a738a5f88ae062599882eaf0df7ed620
Dave <davidpaulturner@gmail.com>
2020-06-12 15:06:59 +0100

    finally fixed error with transferring data

--- RESTful_inference/app.py
+++ RESTful_inference/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify, request, Markup
-from flask_redis import FlaskRedis
-from celery import Celery
 import torch
 from time import perf_counter_ns, time
 import tools.utils as utils
@@ -10,7 +8,6 @@ import torchvision.transforms as transforms
 import torchsummary
 from PIL import Image
 from io import BytesIO
-import energyusage
 
 
 app = Flask(__name__)
@@ -48,26 +45,31 @@ def inference():
 
     """
     # Convery bytes to image
-    im = Image.open(BytesIO(request.data))
+    # print(request.files["files"])
+    im = Image.open(BytesIO(request.files['files'].read()))
     # im.show()
 
     # image preprocessing: convert to tensor and add dimension to mimick batch
     im_tensor = transforms.ToTensor()(im)
+
+    im_tensor = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])(im_tensor)
+
     data = im_tensor.unsqueeze(0)
     data = data.to(utils.DEVICE)
 
-    # run inference
+    # # run inference
     perf_start = perf_counter_ns()
 
-    # inference here
-    top5_values, top5_indicies, raw = get_top5(net, data)
+    # # inference here
+    (top5_values, top5_indicies), raw = get_top5(net, data)
 
     perf_end = perf_counter_ns()
 
     # return results as a json
     return jsonify({
         "result": {
-            "prediction_raw": [x.item() for x in raw[0]],
+            "prediction_raw": raw[0].tolist(),
             "predicted_class": [top5_indicies[0][0].item()],
             "confidence": [top5_values[0][0].item()],
             "top5": [x.item() for x in top5_indicies[0]],
@@ -92,5 +94,10 @@ def model():
     return jsonify(torchsummary.summary(net, (3, 224, 224)))
 
 
+@app.route("/changemodel/<slug>")
+def change_model(model):
+    return(jsonify({"Error": "Not Yet Implimented."}))
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
--- RESTful_inference/tools/utils.py
+++ RESTful_inference/tools/utils.py
@@ -3,7 +3,7 @@
 """
 import torch
 
-DEVICE = torch.device(f'c0' if torch.cuda.is_available() else 'cpu')  # nopep8 pyl disable=no-member
+DEVICE = torch.device(f'cuda' if torch.cuda.is_available() else 'cpu')  # nopep8 pyl disable=no-member
 
 CIFAR10_MODELS = {}
 
@@ -11,5 +11,63 @@ IMAGENET_MODELS = {}
 
 
 def get_top5(net, data):
-    out = net(data)
-    return torch.topk(torch.nn.functional.softmax(out, dim=1), 5)
+    with torch.no_grad():
+        out = net(data)
+        return torch.topk(torch.nn.functional.softmax(out, dim=1), 5), out
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
