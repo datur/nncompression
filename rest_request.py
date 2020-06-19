295878d1b28fa50bf17584dc31f99d51f9a2fedc
TODO <todo-actions[bot]@users.noreply.github.com>
2020-06-19 15:18:01 +0000

    Collect TODO comments

--- rest_request.py
+++ rest_request.py
@@ -95,10 +95,10 @@ with torch.no_grad():
     for i, (images, labels) in enumerate(val_loader):
         
         '''
-        TODO: Experiment with json to binary to deliver the tensor as a list
-        TODO: Record the results somewhere maybe in a json file or a database or tensorboard
-        TODO: Average meter for latency inbound and outbound - this should be the time arrived on server minus time sent. and same for response.
-        TODO: work out accuracy per class
+        TODO [$5eecd7294f395a0007401461]: Experiment with json to binary to deliver the tensor as a list
+        TODO [$5eecd7294f395a0007401462]: Record the results somewhere maybe in a json file or a database or tensorboard
+        TODO [$5eecd7294f395a0007401463]: Average meter for latency inbound and outbound - this should be the time arrived on server minus time sent. and same for response.
+        TODO [$5eecd7294f395a0007401464]: work out accuracy per class
         '''
 
         # load image into pil format
